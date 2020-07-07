721f28b7f03da77054550f7143345c4e30d38e1e
MariaEiriz <mary.eiriz@gmail.com>
2020-07-07 18:45:37 +0200

    SonarCloud

---
+++ sonar.sh
@@ -0,0 +1,6 @@
+#!/bin/dash
+./gradlew sonarqube \
+  -Dsonar.projectKey=MariaEiriz_CS-Recuperacion2020 \
+  -Dsonar.organization=mariaeiriz \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=b6f2f7a3c44495fc9ed23201d15a2a77ade07ee6
