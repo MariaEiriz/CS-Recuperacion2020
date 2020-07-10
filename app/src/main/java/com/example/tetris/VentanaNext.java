8cce2cfab1ae3a54540443f32bb12c99aa766285
MariaEiriz <mary.eiriz@gmail.com>
2020-07-10 11:24:21 +0200

    Revert "Refactorización peor clase (Tablero.java)"
    This reverts commit 91baa91c15ef04e866f6e7a151d447adbb21aa31.

--- app/src/main/java/com/example/tetris/VentanaNext.java
+++ app/src/main/java/com/example/tetris/VentanaNext.java
@@ -32,7 +32,7 @@ public class VentanaNext extends View {
     public void limpiarVentana() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                ventana.getTab()[i][j] = 0;
+                ventana.tab[i][j] = 0;
             }
         }
     }
